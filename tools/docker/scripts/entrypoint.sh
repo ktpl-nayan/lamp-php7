cf061dbc0afb159912aea51343814ebd9d39af27
Krish TechnoLabs <network@krishtechnolabs.com>
2018-06-11 07:49:39 +0000

    Update

--- tools/docker/scripts/entrypoint.sh
+++ tools/docker/scripts/entrypoint.sh
@@ -10,6 +10,7 @@ if [ ! -f docker_initialized ]; then
 	chown -R $dev_user: /var/www/html
 	usermod -a -G adm $dev_user
 	usermod -a -G www-data $dev_user
+	mysql -u root -e \ "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'secret';"
         touch docker_initialized
 fi
 exec "$@"
